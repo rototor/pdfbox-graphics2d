6965f9a4e84eaccfd0b1ecb5ad588fa402c8f3ce
Emmeran Seehuber <rototor@rototor.de>
2017-08-27 23:22:35 +0200

    Copyright stuff.

--- src/main/java/de/rototor/pdfbox/graphics2d/IPdfBoxGraphics2DFontTextDrawer.java
+++ src/main/java/de/rototor/pdfbox/graphics2d/IPdfBoxGraphics2DFontTextDrawer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Emmeran Seehuber
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.rototor.pdfbox.graphics2d;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -66,7 +81,8 @@ public interface IPdfBoxGraphics2DFontTextDrawer {
 	 * @return true when the given text can be fully drawn using fonts. return false
 	 *         to have the text drawn as vector shapes
 	 */
-	boolean canDrawText(AttributedCharacterIterator iterator, IFontTextDrawerEnv env) throws IOException, FontFormatException;
+	boolean canDrawText(AttributedCharacterIterator iterator, IFontTextDrawerEnv env)
+			throws IOException, FontFormatException;
 
 	/**
 	 * @param iterator
