c6418671556532c005a2cdb6e9a9fd1671697242
Larry Lynn <larry.lynn@workiva.com>
2019-10-15 10:50:32 -0600

    try not to touch base if needed.  isolate CMYK stuff into its own test

--- src/test/java/de/rototor/pdfbox/graphics2d/RenderSVGsTest.java
+++ src/test/java/de/rototor/pdfbox/graphics2d/RenderSVGsTest.java
@@ -4,10 +4,18 @@ import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.bridge.*;
 import org.apache.batik.gvt.GraphicsNode;
 import org.apache.batik.util.XMLResourceDescriptor;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.util.Matrix;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class RenderSVGsTest extends PdfBoxGraphics2DTestBase
@@ -87,14 +95,52 @@ public class RenderSVGsTest extends PdfBoxGraphics2DTestBase
         GVTBuilder builder = new GVTBuilder();
         final GraphicsNode gvtRoot = builder.build(bctx, document);
 
-        this.exportGraphicCMYK("svg", name.replace(".svg", ""), new GraphicsExporter()
-        {
-            @Override
-            public void draw(Graphics2D gfx)
-            {
-                gfx.scale(scale, scale);
-                gvtRoot.paint(gfx);
+        try {
+            PDDocument pdfDocument = new PDDocument();
+
+            File parentDir = new File("target/test/svg");
+            // noinspection ResultOfMethodCallIgnored
+            parentDir.mkdirs();
+
+            PDPage page = new PDPage(PDRectangle.A4);
+            pdfDocument.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page);
+
+            PdfBoxGraphics2D pdfBoxGraphics2D = new PdfBoxGraphics2D(pdfDocument, 400, 400);
+            PdfBoxGraphics2DColorMapper colorMapper = new RGBtoCMYKColorMapper();
+            pdfBoxGraphics2D.setColorMapper(colorMapper);
+
+            PdfBoxGraphics2DFontTextDrawer fontTextDrawer = null;
+            contentStream.beginText();
+            contentStream.setStrokingColor(0.0f, 0.0f, 0.0f, 1.0f);
+            contentStream.setNonStrokingColor(0.0f, 0.0f, 0.0f, 1.0f);
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 15);
+            contentStream.setTextMatrix(Matrix.getTranslateInstance(10, 800));
+            contentStream.showText("Mode " + Mode.FontTextIfPossible);
+            contentStream.endText();
+            fontTextDrawer = new PdfBoxGraphics2DFontTextDrawer();
+
+            if (fontTextDrawer != null) {
+                pdfBoxGraphics2D.setFontTextDrawer(fontTextDrawer);
             }
-        });
+
+            pdfBoxGraphics2D.scale(scale, scale);
+            gvtRoot.paint(pdfBoxGraphics2D);
+            pdfBoxGraphics2D.dispose();
+
+            PDFormXObject appearanceStream = pdfBoxGraphics2D.getXFormObject();
+            Matrix matrix = new Matrix();
+            matrix.translate(0, 20);
+            contentStream.transform(matrix);
+            contentStream.drawForm(appearanceStream);
+
+            contentStream.close();
+
+            pdfDocument.save(new File(parentDir, name + ".pdf"));
+            pdfDocument.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
