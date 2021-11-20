bbd27c7cbca5ca184eb36e83aa90a4a75111ccc3
Emmeran Seehuber <rototor@rototor.de>
2021-11-20 20:11:56 +0100

    PDFBOX-5322: Reduced the problem to a miminmal test case.

--- extended-tests/src/test/java/de/rototor/pdfbox/graphics2d/extendedtests/TestGraphicsFillBug.java
+++ extended-tests/src/test/java/de/rototor/pdfbox/graphics2d/extendedtests/TestGraphicsFillBug.java
@@ -6,10 +6,9 @@ import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Path2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -40,29 +39,24 @@ public class TestGraphicsFillBug
     }
 
     @Test
-    public void testBug() throws IOException
+    public void testAntialiasingPathDrawBug() throws IOException
     {
-        BufferedImage image = new BufferedImage(1000, 1000, TYPE_INT_ARGB);
+        BufferedImage image = new BufferedImage(500, 300, TYPE_INT_ARGB);
         Graphics2D g = image.createGraphics();
         g.setBackground(new Color(0xFFFFFFFF));
         g.clearRect(0, 0, 1000, 1000);
-        g.scale(1.0, 1.0);
+
         Map<Object, Object> hints = new HashMap<Object, Object>();
+        // When not setting Antialiasing the bug is gone.
         hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        hints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g.addRenderingHints(hints);
         g.translate(0, 311.0);
         g.scale(1.0, -1.0);
-        g.translate(-0, -0);
 
         g.setPaint(new Color(0xFF000000));
         g.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null,
                 0.0f));
 
-        g.setClip(new Area(new Rectangle2D.Double(0, 0, 611.0, 623.0)));
-
-        //g.scale(1, 1);
         GeneralPath p = new GeneralPath(Path2D.WIND_NON_ZERO);
         p.moveTo(438.531494140625, 261.2290954589844);
         p.curveTo(437.89190673828125, 265.30859375, 437.6903076171875, 265.33380126953125,
@@ -70,14 +64,14 @@ public class TestGraphicsFillBug
         p.curveTo(437.5043029785156, 265.3570861816406, 437.3822021484375, 265.2620849609375,
                 437.3822021484375, 265.2620849609375);
 
-        //g.setPaint(Color.RED);
-        //g.fill(p);
         g.setPaint(Color.BLACK);
-
         g.draw(p);
 
-        //g.drawLine(5, 5, 100, 100);
+        // Just for Orientation: Where is the Path p drown?
+        g.setPaint(Color.RED);
+        g.draw(new Ellipse2D.Float(428, 255, 20, 20));
         g.dispose();
+
         new File("target/test/").mkdirs();
         ImageIO.write(image, "PNG", new File("target/test/buggy.png"));
     }
