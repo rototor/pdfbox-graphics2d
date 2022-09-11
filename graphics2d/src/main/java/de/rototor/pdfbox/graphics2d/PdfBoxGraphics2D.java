249ae4f4c90727580ac86787e402bfc9f893fa9d
Emmeran Seehuber <rototor@rototor.de>
2022-09-11 15:47:16 +0200

    #45 Copy&Paste Error in the drawImage call forwarding.
    sy1 should be passed for sy1, not sy2...
    Thanks @fransbouwmans for pointing this out.

--- graphics2d/src/main/java/de/rototor/pdfbox/graphics2d/PdfBoxGraphics2D.java
+++ graphics2d/src/main/java/de/rototor/pdfbox/graphics2d/PdfBoxGraphics2D.java
@@ -652,7 +652,7 @@ public class PdfBoxGraphics2D extends Graphics2D
     public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1,
             int sx2, int sy2, ImageObserver observer)
     {
-        return drawImage(img, dx1, dy1, dx2, dy2, sx1, sy2, sx2, sy2, null, observer);
+        return drawImage(img, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, null, observer);
     }
 
     public boolean drawImage(Image img, AffineTransform xform, ImageObserver obs)
